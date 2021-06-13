9b54f63c21dc2afa8fb596d03300ad809fbb042b
fimoefive <brothermj@hotmail.com>
2021-06-12 23:47:50 -0500

    inital commit on mjs-auth branch.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 
-import './styles/index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/index.scss';
 
