8313829ede379b46efa13bfb31ce31ce8e456b3a
fimoefive <brothermj@hotmail.com>
2021-06-14 18:57:28 -0500

    Added key: 'values' PropTypes to Holes() and imported HoleForm function into Holes.js file.

--- src/views/Holes.js
+++ src/views/Holes.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Button } from 'reactstrap';
 import HoleCard from '../components/HoleCard';
+import HoleForm from '../forms/HoleForm';
 
-function Hole({ user }) {
+function Holes({ user, holes, setHoles }) {
+  const [showAddHole, setAddHole] = useState(false);
+
+  const handleClick = () => {
+    setAddHole((prevState) => !prevState);
+  };
+
   return (
     <>
       <div className="card-container">
+        <div>
+          {!showAddHole
+            ? <Button className="addHoleBtn" color="primary" user={user} onClick={handleClick}>ADD HOLE</Button>
+            : <div>
+              <Button className="closeForm" color="secondary" onClick={handleClick}>CLOSE</Button>
+              <HoleForm
+                setHoles={setHoles}
+                user={user}
+              />
+            </div>
+          }
+        </div>
         {holes.map((holeInfo) => (
-          <HoleCard
+          <HoleCard className="holeCard"
             key={holeInfo.firebaseKey}
             firebaseKey={holeInfo.firebaseKey}
+            roundNum={holeInfo.roundNum}
+            par={holeInfo.par}
             uid={holeInfo.uid}
             user={user}
+            setHoles={setHoles}
           />
         ))}
       </div>
@@ -19,8 +42,10 @@ function Hole({ user }) {
   );
 }
 
-Hole.propTypes = {
+Holes.propTypes = {
+  holes: PropTypes.array.isRequired,
+  setHoles: PropTypes.func.isRequired,
   user: PropTypes.any
 };
 
-export default Hole;
+export default Holes;
