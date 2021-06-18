3e53183255d28b6a15e1af581e777603ff4ef912
fimoefive <brothermj@hotmail.com>
2021-06-18 15:54:08 -0500

    Added total and avg PropTypes and <div> to HoleCard.js.

--- src/components/HoleCard.js
+++ src/components/HoleCard.js
@@ -25,6 +25,8 @@ const HoleCard = ({
   hole7,
   hole8,
   hole9,
+  total,
+  avg,
   setHoles
 }) => {
   const [editing, setEditing] = useState(false);
@@ -69,6 +71,8 @@ const HoleCard = ({
         <div id='hole8' type='number'>{hole8}</div>
         <CardText type="text">Hole 9</CardText>
         <div id='hole9' type='number'>{hole9}</div>
+        <div id='total' type='number'>Total: {total}</div>
+        <div id='avg' type='number'>Average: {avg}</div>
         <Button color="warning" onClick={() => handleClick('view')}>View Hole</Button>
         <Button color="danger" onClick={() => handleClick('delete')}>Delete Hole</Button>
         <Button color="info" onClick={() => handleClick('edit')}>
@@ -91,6 +95,8 @@ const HoleCard = ({
             hole7={hole7}
             hole8={hole8}
             hole9={hole9}
+            total={total}
+            avg={avg}
             setHoles={setHoles}
           />
         }
@@ -114,6 +120,8 @@ HoleCard.propTypes = {
   hole7: PropTypes.string,
   hole8: PropTypes.string,
   hole9: PropTypes.string,
+  total: PropTypes.string,
+  avg: PropTypes.string,
   setHoles: PropTypes.func
 };
 
