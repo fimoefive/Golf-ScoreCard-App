0c76afd3395227d785e236bfddf0f6710c741eca
fimoefive <brothermj@hotmail.com>
2021-06-21 12:15:40 -0500

    updated userData.json userObject and imported it into Firebase.

--- src/helpers/data/userData.js
+++ src/helpers/data/userData.js
@@ -4,16 +4,16 @@ import firebaseConfig from '../apiKeys';
 const dbUrl = firebaseConfig.databaseURL;
 
 const getUser = () => new Promise((resolve, reject) => {
-  axios.get(`${dbUrl}/users.json`)
+  axios.get(`${dbUrl}/user.json`)
     .then((response) => resolve(Object.values(response.data)))
     .catch((error) => reject(error));
 });
 
 const createUser = (userObject) => new Promise((resolve, reject) => {
-  axios.post(`${dbUrl}/users.json`, userObject)
+  axios.post(`${dbUrl}/user.json`, userObject)
     .then((response) => {
       const body = { firebaseKey: response.data.name };
-      axios.patch(`${dbUrl}/users/${response.data.name}.json`, body)
+      axios.patch(`${dbUrl}/user/${response.data.name}.json`, body)
         .then(() => {
           getUser().then((usersArray) => resolve(usersArray));
         });
@@ -21,7 +21,7 @@ const createUser = (userObject) => new Promise((resolve, reject) => {
 });
 
 const getUserUid = (uid) => new Promise((resolve, reject) => {
-  axios.get(`${dbUrl}/users.json?orderBy="uid"&equalTo="${uid}"`)
+  axios.get(`${dbUrl}/user.json?orderBy="uid"&equalTo="${uid}"`)
     .then((response) => resolve(response))
     .catch((error) => reject(error));
 });
